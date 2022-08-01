3acd36e1124034b52a819e49a145611109f8f405
ludykraks <64922897+ludykraks@users.noreply.github.com>
2022-08-01 11:55:41 +0000

    Add files via upload

---
+++ js/anima.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+   $("#godown").click(function() {
+   $(".content-heading").animate({ fontSize:'32px'
+  }, 800, function() { 
+     $(this).css("color", "#ffffff")}
+    );
+  });
+ }); ;
